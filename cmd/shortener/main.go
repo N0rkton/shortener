3fa2e4c7bbe3f7ff2af05e287ca27b34aba18ccb
N0rkton <shvm20031807@gmail.com>
2022-12-22 23:24:01 +0300

    increment1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,89 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+
+	"log"
+	"math/rand"
+	"net/http"
+	"net/url"
+)
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func shorting() string {
+	b := make([]byte, 5)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+func isValidUrl(token string) bool {
+	_, err := url.ParseRequestURI(token)
+	if err != nil {
+		return false
+	}
+	u, err := url.Parse(token)
+	if err != nil || u.Host == "" {
+		return false
+	}
+	return true
+}
+
+type Result struct {
+	Link   string
+	Code   string
+	Status string
+}
+
+var db []Result
+
+func indexPage(w http.ResponseWriter, r *http.Request) {
+
+	result := Result{}
+	if r.Method == "POST" {
+		if !isValidUrl(r.FormValue("s")) {
+			fmt.Println("Что-то не так")
+			result.Status = "Ссылка имеет неправильный формат!"
+			w.WriteHeader(400)
+			result.Link = ""
+		} else {
+			result.Link = r.FormValue("s")
+			result.Code = shorting()
+			w.WriteHeader(201)
+			w.Header().Set("content-type", "text/plain")
+			w.Write([]byte(result.Code))
+			result.Status = "Сокращение было выполнено успешно"
+		}
+	}
+	db = append(db, result)
+
+}
+
+func redirectTo(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	b := 0
+	for link := range db {
+		if db[link].Code == vars["key"] {
+			b = 1
+			fmt.Fprintf(w, "<script>location='%s';</script>", db[link].Link)
+			w.WriteHeader(307)
+			w.Header().Set("Location", db[link].Link)
+			break
+		}
+	}
+	if b == 0 {
+		w.WriteHeader(400)
+	}
+
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", indexPage)
+	router.HandleFunc("/to/{key}", redirectTo)
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
