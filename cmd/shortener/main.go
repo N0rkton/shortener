ec6e52b894ffbfef409d29bde416c635a0206f09
N0rkton <shvm20031807@gmail.com>
2023-01-22 23:47:44 +0300

    increment7

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"encoding/json"
+	"flag"
 	"github.com/N0rkton/shortener/internal/app/storage"
 	"github.com/gorilla/mux"
 	"io"
@@ -14,30 +15,29 @@ import (
 )
 
 func jsonIndexPage(w http.ResponseWriter, r *http.Request) {
-	var b body
-	err := json.NewDecoder(r.Body).Decode(&b)
+	var body body
+	err := json.NewDecoder(r.Body).Decode(&body)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	if !isValidURL(b.URL) {
+	if !isValidURL(body.URL) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 	code := generateRandomString()
-	ok := db.AddURL(code, b.URL)
+	ok := db.AddURL(code, body.URL)
 	if ok != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	writeToFile(code, b.URL)
+	writeToFile(code, body.URL)
 	w.Header().Set("content-type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	baseURL := os.Getenv("BASE_URL")
 	var res response
 	if baseURL == "" {
-		res.Result = addr + code
-
+		res.Result = *b + code
 	} else {
 		res.Result = baseURL + "/" + code
 	}
@@ -66,7 +66,7 @@ func indexPage(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	baseURL := os.Getenv("BASE_URL")
 	if baseURL == "" {
-		w.Write([]byte(addr + code))
+		w.Write([]byte(*b + code))
 		return
 	}
 	w.Write([]byte(baseURL + "/" + code))
@@ -90,9 +90,24 @@ const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567
 const urlLen = 5
 const addr = "http://localhost:8080/"
 
+var (
+	a *string
+	b *string
+	f *string
+)
+
+func init() {
+	a = flag.String("a", "localhost:8080", "Domain name")
+	b = flag.String("b", addr, "port number")
+	f = flag.String("f", "", "file path")
+}
+
 func readFromFile() {
 	fileStoragePath := os.Getenv("FILE_STORAGE_PATH")
 	var text map[string]string
+	if fileStoragePath == "" {
+		fileStoragePath = *f
+	}
 	if fileStoragePath != "" {
 		file, err := os.OpenFile(fileStoragePath, os.O_RDONLY|os.O_CREATE, 0777)
 		if err != nil {
@@ -149,14 +164,16 @@ type response struct {
 var db storage.Storage
 
 func main() {
-	serverAddress := os.Getenv("SERVER_ADDRESS")
-	if serverAddress == "" {
-		serverAddress = "localhost:8080"
-	}
+	flag.Parse()
 	db = storage.NewMemoryStorage()
 	router := mux.NewRouter()
 	router.HandleFunc("/", indexPage).Methods(http.MethodPost)
 	router.HandleFunc("/api/shorten", jsonIndexPage).Methods(http.MethodPost)
 	router.HandleFunc("/{id}", redirectTo).Methods(http.MethodGet)
-	log.Fatal(http.ListenAndServe(serverAddress, router))
+	serverAddress := os.Getenv("SERVER_ADDRESS")
+	if serverAddress == "" {
+		log.Fatal(http.ListenAndServe(*a, router))
+	} else {
+		log.Fatal(http.ListenAndServe(serverAddress, router))
+	}
 }
